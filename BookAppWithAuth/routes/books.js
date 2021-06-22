57734154b01cd2799b9d3027a4c3f0884931d1ab
dee-na7721 <deenasitikhu@gmail.com>
2021-06-22 11:39:22 +0545

    Search Feature Added

--- BookAppWithAuth/routes/books.js
+++ BookAppWithAuth/routes/books.js
@@ -3,13 +3,17 @@ var router = express.Router();
 var books = require("../resources/books");
 let Books = require("../models/books");
 const { findOneAndUpdate } = require("../models/books");
+const { route } = require(".");
+const { render } = require("../app");
 
+//Get add book page
 router.get("/add", function (req, res, next) {
   res.render("addBooks", {
     title: "Add book",
   });
 });
 
+//Save created book
 router.post("/save", function (req, res) {
   const book = new Books(req.body);
   let promise = book.save();
@@ -19,18 +23,21 @@ router.post("/save", function (req, res) {
   });
 });
 
+//Delete books
 router.get("/remove/:id", function (req, res) {
   Books.remove({ _id: req.params.id }, function () {
     res.redirect("/");
   });
 });
 
+//Get edit page
 router.get("/edit/:_id", function (req, res) {
   Books.findOne({ _id: req.params._id }, function (err, book) {
     res.render("editBooks", { title: "Edit Book", book: book });
   });
 });
 
+//Post changes
 router.post("/edit/:_id", function (req, res) {
   Books.findOneAndUpdate(
     { _id: req.params._id },
@@ -41,5 +48,50 @@ router.post("/edit/:_id", function (req, res) {
     }
   );
 });
+
+//Search books
+// router.get("/search", (req, res) => {
+//   res.render("search");
+// });
+
+// //
+// router.get("/searchbooks/:searchitem", (req, res) => {
+//   var regex = new RegExp(req.params.searchitem, "i");
+
+//   try {
+//     Books.find({
+//       $or: [{ title: regex }, { author: regex }, { genre: regex }],
+//     }).then((result) => {
+//       res.status(200).json(result);
+//       // watch here
+//       res.render("search", { bookList: result });
+//     });
+//   } catch (err) {
+//     res.status.send(err);
+//   }
+
+// var searchItem = req.params.searchitem;
+// try {
+//   Books.find(
+//     {
+//       $or: [
+//         { title: { $regex: `${searchItem}`, options: "$i" } },
+//         // { author: { $regex: req.params.search, $options: "$i" } },
+//         // { description: { $regex: req.params.search, $options: "$i" } },
+//         // { genre: { $regex: req.params.search, $options: "$i" } },
+//       ],
+//     },
+//     (err, data) => {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         res.render("search", { message: "testing", data: data });
+//       }
+//     }
+//   );
+// } catch (error) {
+//   console.log(error);
+// }
+// });
 
 module.exports = router;
--- BookAppWithAuth/routes/index.js
+++ BookAppWithAuth/routes/index.js
@@ -1,20 +1,65 @@
 var express = require("express");
 var router = express.Router();
-var books = require("../resources/books");
+// var books = require("../resources/books");
 let Books = require("../models/books");
+
+function escapeRegex(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  // Books.find({}, function(err, books) {
-  let books = await Books.find();
-  // if (!err) {
-  res.render("index", {
-    title: "Book App",
-    bookList: books,
-    isLoggedIn: req.isAuthenticated(),
-    logoutMessage: req.flash("logoutmessage"), //from logout
-    // loginMessage: req.flash(passport.successFlash),
-  });
+  // eval(require("locus"));
+
+  var noMatch = 0;
+  var noMatchmsg = "";
+  //Search books
+  if (req.query.search) {
+    const regex = new RegExp(escapeRegex(req.query.search), "gi");
+    //Get searched books from db
+    Books.find()
+      .or([{ title: regex }, { author: regex }, { genre: regex }])
+      .exec(function (err, bookSearched) {
+        if (err) {
+          console.log(err);
+        } else {
+          if (bookSearched.length < 1) {
+            console.log("No book match your query, Please try again!!");
+            noMatchmsg = "No book match your query, Please try again!";
+            noMatch = 1;
+            res.render("index", {
+              title: "Your Books",
+              bookList: bookSearched,
+              noMatch: noMatch,
+              noMatchmsg: noMatchmsg,
+              isLoggedIn: req.isAuthenticated(),
+              logoutMessage: req.flash("logoutmessage"), //from logout
+            });
+            // eval(require("locus"));
+          } else {
+            res.render("index", {
+              title: "Your Books",
+              bookList: bookSearched,
+              noMatch: noMatch,
+              noMatchmsg: noMatchmsg,
+              isLoggedIn: req.isAuthenticated(),
+              logoutMessage: req.flash("logoutmessage"), //from logout
+            });
+          }
+        }
+      });
+  } else {
+    let books = await Books.find();
+    res.render("index", {
+      title: "Book App",
+      bookList: books,
+      noMatch: noMatch,
+      noMatchmsg: noMatchmsg,
+      isLoggedIn: req.isAuthenticated(),
+      logoutMessage: req.flash("logoutmessage"), //from logout
+      // loginMessage: req.flash(passport.successFlash),
+    });
+  }
 });
 
 router.get("/deena", function (req, res, next) {
